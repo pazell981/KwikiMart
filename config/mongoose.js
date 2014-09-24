72703a33e87fd699d5ff69e469fbb3b194703b7b
Paul Zellmer <paulzellmer9@gmail.com>
2014-09-23 21:31:57 -0700

    db fix

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose')
 // Connect to mongodb
 var connect = function () {
   var options = { server: { socketOptions: { keepAlive: 1 } } }
-  mongoose.connect('mongodb://heroku:5af3d871f65d584d87fcc3d13caa867a@kahana.mongohq.com:10007/app29942847', options)
+  mongoose.connect('mongodb://heroku:Idi1P9BmWC7H1o2QvMkBAdYeQ5sFM7cpuwBL55PQmm91ngH4axovJFnz-jMcFiApkKMkejoycFXKQ7CuXRvFZQ@kahana.mongohq.com:10007/app29942847
+', options)
 }
 connect();
 // Error handler
